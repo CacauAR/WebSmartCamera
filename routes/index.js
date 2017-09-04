276d3a957753818c81bcc765f04f2eb515b9b562
Vanessa <vanessa.cr.vasconcelos@gmail.com>
2017-09-04 14:29:29 -0300

    login funcionando

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
  app.get('/', function(req, res) {
    res.render('pages/index');
  });
- app.get('/about', function(req, res) {
-   res.render('pages/about');
+ app.get('/login', function(req, res) {
+   res.render('pages/login');
  });
 };

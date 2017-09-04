9c468846675efca783892fa0b1a89dd3b15eeacd
CacauAR <vanessa.cr.vasconcelos@gmail.com>
2017-09-04 14:54:35 -0300

    video funcionando

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 'use strict';
 module.exports = function(app) {
- app.get('/', function(req, res) {
-   res.render('pages/index');
- });
- app.get('/login', function(req, res) {
-   res.render('pages/login');
- });
+  app.get('/', function(req, res) {
+    res.render('pages/index');
+  });
+  app.get('/login', function(req, res) {
+    res.render('pages/login');
+  });
+  app.get('/video', function(req, res) {
+    res.render('pages/video');
+  });
 };

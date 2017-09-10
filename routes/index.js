025b0ad4e1bd8b75743506e4460bba8b426b70da
CacauAR <claudia_rodrigues11@hotmail.com>
2017-09-10 19:27:44 -0300

    Test Create Database

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,15 @@ module.exports = function(app,passport) {
   app.get('/calendar', function(req, res) {
     res.render('pages/calendar');
   });
-  //app.get('/', function(req, res) {
-    //res.render('pages/index');
-  //});
   app.get('/listadisciplinas', function(req, res) {
     res.render('pages/listadisciplinas');
   });
-  //app.get('/login', function(req, res) {
-    //res.render('pages/login');
-  //});
   app.get('/messages', function(req, res) {
     res.render('pages/messages');
   });
-  //app.get('/profile', function(req, res) {
-    //res.render('pages/profile');
-  //});
   app.get('/register', function(req, res) {
     res.render('pages/register');
   });
-  //app.get('/signup', function(req, res) {
-    //res.render('pages/signup');
-  //});
   app.get('/submenu', function(req, res) {
     res.render('pages/submenu');
   });

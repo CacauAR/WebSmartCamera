0837fc053e524ebd379815bf2851942d09a2a30e
vanessa <vanessa.cr.vasconcelos@gmail.com>
2017-09-27 23:30:34 -0300

    transmissao arquivo de audio e video

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 //res.render() will look in a views folder for the view
 module.exports = function (app, passport) {
 
@@ -45,6 +47,52 @@ module.exports = function (app, passport) {
       title: "WebSmartCamera - Aula ao Vivo" 
     });
   });
+
+  app.get('/assistir', isLoggedIn, function (req, res, next) {
+    res.render('pages/assistir', {
+      user: req.user,
+      title: "WebSmartCamera - AULA TAL" 
+    });
+    next();
+  });
+
+
+  app.get('/movies/:movieName', isLoggedIn, function (req, res) {
+
+   const { movieName } = req.params;
+   const movieFile = `./movies/${movieName}`;
+
+   fs.stat(movieFile, (err, stats) => {
+     if (err) {
+       console.log(err);
+       return res.status(404).end('<h1>Movie Not found</h1>');
+     }
+     // Variáveis necessárias para montar o chunk header corretamente
+     const { range } = req.headers;
+     const { size } = stats;
+     const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+     const end = size - 1;
+     const chunkSize = (end - start) + 1;
+     // Definindo headers de chunk
+     res.set({
+       'Content-Range': `bytes ${start}-${end}/${size}`,
+       'Accept-Ranges': 'bytes',
+       'Content-Length': chunkSize,
+       'Content-Type': 'video/mp4'
+     });
+     // É importante usar status 206 - Partial Content para o streaming funcionar
+     res.status(206);
+     // Utilizando ReadStream do Node.js
+     // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
+     const stream = fs.createReadStream(movieFile, { start, end });
+     stream.on('open', () => stream.pipe(res));
+     stream.on('error', (streamErr) => res.end(streamErr));
+   });
+ });
+ app.listen(3000, () => console.log('VideoFlix Server!'));
+
+
+
 
   // =================================================
   // LOGIN - HOME PAGE ===============================
